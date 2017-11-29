37e5cb44bae586ae308913cda59a27a7525b4a0b
remuspopasepa <remus.popa@sepa.at>
2017-11-29 19:14:46 +0200

    Commit intermediar-4 --http://jira.sepa.at/browse/HFP-55
    Commit intermediar-4 --http://jira.sepa.at/browse/HFP-55

--- local/modules/FilterConfigurator/Loop/FilterFeaturesValue.php
+++ local/modules/FilterConfigurator/Loop/FilterFeaturesValue.php
@@ -1,6 +1,7 @@
 <?php
 namespace FilterConfigurator\Loop;
 
+use Propel\Runtime\ActiveQuery\ModelCriteria;
 use Thelia\Core\Template\Element\BaseI18nLoop;
 use Thelia\Core\Template\Element\LoopResult;
 use Thelia\Core\Template\Element\LoopResultRow;
@@ -45,7 +46,8 @@ class FilterFeaturesValue extends BaseI18nLoop implements PropelSearchLoopInterf
                           ->set('AAA',$listing->getVirtualColumn('aaa')); */
             //\var_dump($listing);
             $loopResultRow->set('FEAT_AV_ID', $listing->getVirtualColumn('FeatureAvID'))
-                          ->set(FEAT_AV_I18n_TITLE,$listing->getVirtualColumn('FeatureAvI18nTITLE'));
+                          ->set(FEAT_AV_I18n_TITLE,$listing->getVirtualColumn('FeatureAvI18nTITLE'))
+                          ->set(FEAT_AV_I18n_DESCRIPTION,$listing->getVirtualColumn('FeatureAvI18nDescription'));
                          
             $loopResult->addRow($loopResultRow);
             
@@ -95,10 +97,12 @@ class FilterFeaturesValue extends BaseI18nLoop implements PropelSearchLoopInterf
             
         
         $search = FeatureAvQuery::create()
+            ->setFormatter(ModelCriteria::FORMAT_ON_DEMAND)
             ->where(FeatureAvTableMap::FEATURE_ID.' = ?', $feature_id, \PDO::PARAM_STR)
             ->withColumn(FeatureAvTableMap::ID, 'FeatureAvID')
             ->addJoin(FeatureAvTableMap::ID, FeatureAvI18nTableMap::ID, \Propel\Runtime\ActiveQuery\Criteria::JOIN)
             ->withColumn(FeatureAvI18nTableMap::TITLE, 'FeatureAvI18nTITLE')
+            ->withColumn(FeatureAvI18nTableMap::DESCRIPTION, 'FeatureAvI18nDescription')
             ->where(FeatureAvI18nTableMap::LOCALE.' = ?', 'de_DE', \PDO::PARAM_STR);
             
         return $search;
