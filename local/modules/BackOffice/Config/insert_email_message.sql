b8e5dfc00e1a0762a1159796a1633d502ee7d808
alisstranici <alis.stranici@sepa.at>
2018-02-14 14:00:27 +0200

    HFP-115
    send email when order status change to pickup

--- local/modules/BackOffice/Config/insert_email_message.sql
+++ local/modules/BackOffice/Config/insert_email_message.sql
@@ -10,3 +10,14 @@ INSERT INTO `message_i18n` (`id`,`locale`, `title`, `subject`) VALUES
 (@last_id_in_message,'de_DE','Message sent to the shop owner when a order failed', 'Zahlungsfehler bei Bestellung Nr. {$order_id}');
 
 INSERT INTO `config` (`name`, `value`, `secured`, `hidden`, `created_at`, `updated_at`) VALUES ('pdf_orders_file', 'orders', '0', '0', '2018-02-05 15:17:47', '2018-02-05 15:17:47');
+
+INSERT INTO `message` (`name`, `secured`, `text_layout_file_name`, `text_template_file_name`, `html_layout_file_name`, `html_template_file_name`, `created_at`, `updated_at`) VALUES
+('pickup_order', NULL, NULL, 'pickup_order.txt', NULL, 'pickup_order.html', NOW(), NOW());
+
+SET @last_id_in_message = LAST_INSERT_ID();
+
+INSERT INTO `message_i18n` (`id`,`locale`, `title`, `subject`) VALUES
+(@last_id_in_message,'en_US','Message sent to the customer when the order can be picked up', 'Your order Nº {$order_id} is ready for pickup');
+
+INSERT INTO `message_i18n` (`id`,`locale`, `title`, `subject`) VALUES
+(@last_id_in_message,'de_DE','Message sent to the customer when the order can be picked up', 'Ihre Bestellung Nr. {$order_id} ist abholbereit');
---
+++ local/modules/BackOffice/Listener/BackOfficeListener.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace BackOffice\Listener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Thelia\Action\BaseAction;
+use Thelia\Core\Event\Order\OrderEvent;
+use Thelia\Core\Event\TheliaEvents;
+use Thelia\Core\HttpFoundation\Request;
+use Thelia\Mailer\MailerFactory;
+
+class BackOfficeListener extends BaseAction implements EventSubscriberInterface {
+
+    protected $request;
+
+    public function __construct(Request $request) {
+        $this->request = $request;
+    }
+
+    /**
+     * Send email to the customer when the orders status changes to pickup
+     * 
+     * @param  OrderEvent $event
+     */
+    public function sendEmailPickupStatusOrder(OrderEvent $event) {
+
+        if ($event->getOrder()->getStatusId() == 12) {
+
+            $messageParameters = array(
+                'order_id' => $event->getOrder()->getId()
+            );
+
+            $parser = $this->getParser($this->getTemplateHelper()->getActiveMailTemplate());
+            $mailer = new MailerFactory($this->getDispatcher(), $parser);
+
+            $mailer->sendEmailMessage(
+                    'pickup_order', 
+                    [ConfigQuery::getStoreEmail() => ConfigQuery::getStoreName()], 
+                    [$event->getOrder()->getCustomer()->getEmail() => $event->getOrder()->getCustomer()->getFirstname() . " " . $event->getOrder()->getCustomer()->getLastname()], 
+                    $messageParameters
+            );
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
+     *
+     * @api
+     */
+    public static function getSubscribedEvents() {
+        return array(
+            TheliaEvents::ORDER_UPDATE_STATUS => array("sendEmailPickupStatusOrder", 128),
+        );
+    }
+
+}
