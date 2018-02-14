b5e0bdcecde52d0e87192fe0c7eb5071768bd973
emanuelplopu <emanuel.plopu@sepa.at>
2018-02-14 20:10:32 +0100

    HFP-119
    Csv filename now has date added at the end and is saved in session in order to permit multiple concomitant requests

--- local/modules/BackOffice/Controller/Admin/ExportDataFromMyshtController.php
+++ local/modules/BackOffice/Controller/Admin/ExportDataFromMyshtController.php
@@ -34,13 +34,21 @@ class ExportDataFromMyshtController extends BaseAdminController {
     protected $version = "2017";
     protected $info = [];
     protected $cookiefile = THELIA_LOCAL_DIR . "config" . DS . "cookies" . DS . "myshtcookieexport.txt";
-    protected $csvFile = THELIA_LOCAL_DIR . "config" . DS . "cookies" . DS . "temp" . DS . "exportCsvDataMysht.csv";
+    protected $csvFileLocation = THELIA_LOCAL_DIR . "config" . DS . "cookies" . DS . "temp" . DS;
+    protected $csvFilename = "";
     protected $imageLocation = THELIA_LOCAL_DIR . "config" . DS . "cookies" . DS . "temp" . DS . "images" . DS;
     protected $imageZip = THELIA_LOCAL_DIR . "config" . DS . "cookies" . DS . "temp" . DS . "images.zip";
-
+    const MYSHT_CSV_FILE = 'exportCsvDataMysht';
+    
     public function export() {
-
+        
         if ($this->getRequest()->get("idartikels")) {
+            /** @var Session $session */
+            $session = $this->getRequest()->getSession();
+            $date = date('m.d.Y.h.i.s.a', time());
+            $this->csvFilename = $this->csvFileLocation.self::MYSHT_CSV_FILE.$date.".csv";
+            $session->set(self::MYSHT_CSV_FILE, $this->csvFilename);
+            $this->initCsvFile($this->csvFilename);
             $this->logout();
             $idartikels = explode(',', $this->getRequest()->get("idartikels"));
 
@@ -72,7 +80,7 @@ class ExportDataFromMyshtController extends BaseAdminController {
             }
             $zip->close();
 
-            $this->exportToCsv();
+            
             $this->logout();
         }
 
@@ -146,11 +154,13 @@ class ExportDataFromMyshtController extends BaseAdminController {
 
             if (isset($response["data"]) && $response["data"]) {
                 $this->setLogger()->error("getArtNr - idartikel = $idartikel response: " . json_encode($response));
-                array_push($this->info, array("idartikel" => $idartikel, "MegabildNr" => $response["data"][0]["MegabildNr"], "Lieferantename" => $response["data"][0]["Lieferantename"],
+                $arrayData = array("idartikel" => $idartikel, "MegabildNr" => $response["data"][0]["MegabildNr"], "Lieferantename" => $response["data"][0]["Lieferantename"],
                     "title" => $response["data"][0]["Zeile1"],
                     "description" => $response["data"][0]["Zeile2"] . " " . $response["data"][0]["agzeile1"],
                     "stock" => $response["data"][0]["SAPLiefermenge"] ? $response["data"][0]["SAPLiefermenge"] : 0,
-                    "price" => $response["data"][0]["aktpreis"]));
+                    "price" => $response["data"][0]["aktpreis"]);
+                
+                $this->exportToCsv($this->csvFilename, $arrayData);
                 return $response["data"][0]["MegabildNr"];
             } else {
                 $this->setLogger()->error("getArtNr - idartikel = $idartikel servererror: " . json_encode($response));
@@ -204,16 +214,16 @@ class ExportDataFromMyshtController extends BaseAdminController {
         @unlink($this->cookiefile);
     }
 
-    function exportToCsv() {
-        $fp = fopen($this->csvFile, 'w');
-
+    function initCsvFile($file){
+        $fp = fopen($file, 'w');
         $fields = array("idartikel", "MegabildNr", "Lieferantename", "title", "description", "stock", "price");
         fputcsv($fp, $fields);
-
-        foreach ($this->info as $fields) {
-            fputcsv($fp, $fields);
-        }
-
+        fclose($fp);
+    }
+    
+    function exportToCsv($csvFile, $arrayData) {
+        $fp = fopen($csvFile, 'a');
+        fputcsv($fp, $arrayData);
         fclose($fp);
     }
 
@@ -222,11 +232,19 @@ class ExportDataFromMyshtController extends BaseAdminController {
         if (null !== $response = $this->checkAuth(AdminResources::MODULE, 'atos', AccessManager::UPDATE)) {
             return $response;
         }
+        $session = $this->getRequest()->getSession();
+        if( $session->has(self::MYSHT_CSV_FILE) == true) {
+            $file = $session->get(self::MYSHT_CSV_FILE);
+            $filePath = explode(DS,$file);
+            $filename = $filePath[sizeof($filePath)-1];
+            return Response::create(@file_get_contents($file), 200, array(
+                'Content-type' => "text/plain",
+                'Content-Disposition' => sprintf("Attachment;filename=".$filename)
+            ));
+        } else {
+            return $this->errorPage($this->getTranslator()->trans("No csv file has been found"), 403);
+        }
 
-        return Response::create(@file_get_contents($this->csvFile), 200, array(
-                    'Content-type' => "text/plain",
-                    'Content-Disposition' => sprintf('Attachment;filename=export-csv-data-mysht.csv')
-        ));
     }
 
     function downloadImages() {
