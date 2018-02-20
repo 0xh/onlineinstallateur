6af9a407202961730427400380d6fce682287fee
emanuelplopu <emanuel.plopu@sepa.at>
2018-02-20 10:02:45 +0100

    commit backoffice from server

---
+++ local/modules/BackOffice/I18n/backOffice/default/de_DE.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'Download pdf' => 'Rechnung als PDF herunterladen',
+    'Export orders to pdf file' => 'Rechnungen als PDF exportieren und herunterladen',
+    'Get orders to pdf file' => 'Rechnungen als PDF exportieren',
+    'Get pdf' => 'Rechnung als PDF herunterladen',
+);
