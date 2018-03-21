4297e3578c7fa9d6bc5cd9fa8569f19c4a46a5be
alisstranici <alis.stranici@sepa.at>
2018-03-21 05:16:58 +0200

    change message with ajax, hook for the button

---
+++ local/modules/WishList/Config/insert_hooks.sql
@@ -0,0 +1,4 @@
+INSERT INTO `hook` 
+( `code`, `type`, `by_module`, `activate`, `created_at`, `updated_at`) 
+VALUES ( "wish-list.wish-button", 1, 0, 1, now(), now())
+ON DUPLICATE KEY UPDATE id = id;
