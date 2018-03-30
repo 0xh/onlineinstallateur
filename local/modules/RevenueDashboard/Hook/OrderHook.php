51bd7cb3511d0ff7a4e42749a6b7347423d196e0
FlorianCatana <florian.catana@sepa.at>
2018-03-30 15:40:02 +0300

    Fulfilment center order

--- local/modules/RevenueDashboard/Hook/OrderHook.php
+++ local/modules/RevenueDashboard/Hook/OrderHook.php
@@ -25,19 +25,19 @@ class OrderHook extends BaseHook {
         $prodId = $event->getArgument('order_product_id');
         $price = number_format($event->getArgument('price'), 2, '.', '');
 
-        $arrPartner = $this->getPriceForPartner($prodId);
+        $arrPartner = $this->getPriceForPartner($prodId, $orderId);
         $event->add(
                 $this->render('order-hook.html', array("arrPartner" => $arrPartner, "prodId" => $prodId,
-                    "partner_selected" => $arrPartner[0]["partner_selected"], "service_price" => $arrPartner[0]["service_price"],"orderId" => $orderId, "price" => $price))
+                    "partner_selected" => $arrPartner[0]["partner_selected"], "service_price" => $arrPartner[0]["service_price"], "orderId" => $orderId, "price" => $price))
         );
     }
 
-    private function getPriceForPartner($prodId) {
+    private function getPriceForPartner($prodId, $orderId) {
 
         $prod = WholesalePartnerProductQuery::create()
                 ->addJoin(WholesalePartnerProductTableMap::PRODUCT_ID, ProductTableMap::ID)
                 ->addJoin(WholesalePartnerProductTableMap::PARTNER_ID, WholesalePartnerTableMap::ID)
-                ->addJoin(ProductTableMap::REF, OrderProductRevenueTableMap::PRODUCT_REF, Criteria::LEFT_JOIN)
+                ->addJoin(ProductTableMap::REF, OrderProductRevenueTableMap::PRODUCT_REF)
                 ->withColumn(WholesalePartnerTableMap::ID, 'partner_id')
                 ->withColumn(WholesalePartnerTableMap::NAME, 'partner_name')
                 ->withColumn(ProductTableMap::REF, 'ref')
@@ -45,7 +45,25 @@ class OrderHook extends BaseHook {
                 ->withColumn(OrderProductRevenueTableMap::PARTNER_ID, 'partner_selected')
                 ->withColumn(OrderProductRevenueTableMap::PURCHASE_PRICE, 'service_price')
                 ->withColumn(WholesalePartnerProductTableMap::DISCOUNT, 'discount')
+                ->where(OrderProductRevenueTableMap::ORDER_ID . "=" . $orderId)
                 ->findByProductId($prodId);
+
+        $partner_selected = 0;
+
+        if (!$prod->getData()) {
+            $prod = WholesalePartnerProductQuery::create()
+                    ->addJoin(WholesalePartnerProductTableMap::PRODUCT_ID, ProductTableMap::ID)
+                    ->addJoin(WholesalePartnerProductTableMap::PARTNER_ID, WholesalePartnerTableMap::ID)
+                    ->withColumn(WholesalePartnerTableMap::ID, 'partner_id')
+                    ->withColumn(WholesalePartnerTableMap::NAME, 'partner_name')
+                    ->withColumn(ProductTableMap::REF, 'ref')
+                    ->withColumn(ProductTableMap::ID, 'prod_id')
+                    ->withColumn(WholesalePartnerProductTableMap::PARTNER_ID, 'partner_selected')
+                    ->withColumn(WholesalePartnerProductTableMap::PRICE, 'service_price')
+                    ->withColumn(WholesalePartnerProductTableMap::DISCOUNT, 'discount')
+                    ->findByProductId($prodId);
+            $partner_selected = -1;
+        }
 
         $arrPartner = array();
 
@@ -55,7 +73,7 @@ class OrderHook extends BaseHook {
                 "partner_name" => $value->getVirtualColumn("partner_name"),
                 "prod_ref" => $value->getVirtualColumn("ref"),
                 "prod_id" => $value->getVirtualColumn("prod_id"),
-                "partner_selected" => $value->getVirtualColumn("partner_selected"),
+                "partner_selected" => $partner_selected === -1 ? $partner_selected : $value->getVirtualColumn("partner_selected"),
                 "service_price" => $value->getVirtualColumn("service_price"),
                 "discount" => $value->getVirtualColumn("discount"),
                 "purchase_price" => $value->getPrice() - ($value->getPrice() * $value->getVirtualColumn("discount") / 100)
