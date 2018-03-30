79d48d7238a3e5edd1df91278de4ef0310be4476
alisstranici <alis.stranici@sepa.at>
2018-03-30 12:23:27 +0300

    triwork tracking number

--- local/modules/SepaImporter/Import/TriworxTrackingOrders.php
+++ local/modules/SepaImporter/Import/TriworxTrackingOrders.php
@@ -6,62 +6,39 @@ use Thelia\Core\Event\TheliaEvents;
 use Thelia\ImportExport\Import\AbstractImport;
 use Thelia\Log\Tlog;
 use Thelia\Model\OrderQuery;
-use Thelia\Mailer\MailerFactory;
 use Thelia\Model\CustomerQuery;
 
 class TriworxTrackingOrders extends AbstractImport {
 
     protected $mandatoryColumns = [
-        'order_id',
-        'order_tracking_number'
+        'LoadingPointName3',
+        'PLCColloCodes'
     ];
-    
-    /** @var MailerFactory  */
-    protected $mailer;
-    
-    public function __construct(MailerFactory $mailer)
-    {
-    	$this->mailer = $mailer;
-    }
-
+ 
     public function importData(array $data) {
         /** @var EventDispatcherInterface $eventDispatcher */
         $eventDispatcher = $this->getContainer()->get('event_dispatcher');
 
-       /*  $orderId = $data['order_id'];
-        $orderTrackingNumber = $data['order_tracking_number']; */
-        
-        $orderId = '6042';
-        $orderTrackingNumber = '23456';
+        $orderRef = $data['LoadingPointName3'];
+        $trackingCode = explode(",", $data['PLCColloCodes']);
+        $orderTrackingNumber = $trackingCode[0]; 
         
         $order = OrderQuery::create()
-        		->findOneById($orderId);
+        	->findOneByRef($orderRef);
         
        	if ($order!== null) {
             //Tlog::getInstance()->info("FeatAVID" . $data['feature_av_id']);
        		$order->setDeliveryRef($orderTrackingNumber)
        			->save();
            
-            //$eventDispatcher->dispatch(TheliaEvents::PRODUCT_FEATURE_UPDATE_VALUE, $productfeatureupdate);
         }
         
         $customer = CustomerQuery::create()
         				->findOneById($order->getCustomerId());
         
-        $this->sendEmailPickupStatusOrder($customer);
+      /* 	$amazonAPI = new AmazonAWSController();
+        $infoAmazon = $amazonAPI->getProductInfoFromAmazon($EAN_code); */
     }
     
-    public function sendEmailPickupStatusOrder($customer)
-    {
-    	
-    	$this->mailer->sendEmailToCustomer(
-    			'pickup_order',
-    			$customer,
-    			[
-    					'order_id' => '6042',
-    					'order_ref' => 'ORD000000001932'
-    			]
-    			);
-    }
 
 }
