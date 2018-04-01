c2aac0fc4b86c711a6b474069e1a53890fd1d2e5
alisstranici <alis.stranici@sepa.at>
2018-04-02 02:31:34 +0300

    send email when the tracking number is saved from backoffice

--- local/modules/SepaImporter/Listener/ImportListener.php
+++ local/modules/SepaImporter/Listener/ImportListener.php
@@ -9,6 +9,8 @@ use SepaImporter\Event\ImportEvent;
 use Thelia\Model\OrderQuery;
 use Thelia\Model\CustomerQuery;
 use Thelia\Log\Tlog;
+use Thelia\Core\Event\TheliaEvents;
+use Thelia\Core\Event\Order\OrderEvent;
 
 class ImportListener extends BaseAction implements EventSubscriberInterface 
 {
@@ -22,7 +24,7 @@ class ImportListener extends BaseAction implements EventSubscriberInterface
     }
 
     /**
-     * Send email to the customer with the order tracking informations
+     * Send email to the customer with the order tracking informations when the tracking number is saved from Triworx import
      * 
      * @param  ImportEvent $event
      */
@@ -32,32 +34,52 @@ class ImportListener extends BaseAction implements EventSubscriberInterface
     				->findOneById($event->getOrderId());
     	
     	if($order) {
+    		$this->sendEmail($order);
+    	}
+    }
+    
+    /**
+     * Send email to the customer with the order tracking informations when the tracking number is saved from BackOffice
+     *
+     * @param  OrderEvent $event
+     */
+    public function sendEmailTrackingOrderBackOffice(OrderEvent $event)
+    {
+	    $order = OrderQuery::create()
+	    			->findOneById($event->getOrder()->getId());
+	    
+	    if($order) {
+	    	$this->sendEmail($order);
+	    }
+    }
+    
+    public function sendEmail($order) {
+    	
+    	$customer = CustomerQuery::create()
+    					->findOneById($order->getCustomerId());
+    	
+    	if($customer) {
+    		Tlog::getInstance()->info("Update order delivery ref - customer id: ".$order->getCustomerId());
     		
-	    	$customer = CustomerQuery::create()
-	    					->findOneById($order->getCustomerId()); 
-	    	
-	    	if($customer) {
-	    		Tlog::getInstance()->info("Triworx Import - customer id: ".$order->getCustomerId());
-	    		
-	            $this->mailer->sendEmailToCustomer(
-	                'customer_traking_order',
-	            	$customer,
-	                [
-	                	'order_id' => $order->getId(),
-	                	'order_ref' => $order->getRef()
-	                ]
-	            ); 
-	    	}
-	    	else {
-	    		Tlog::getInstance()->info("Triworx Import - customer id: ".$order->getCustomerId().' was not found in database');
-	    	}
+    		$this->mailer->sendEmailToCustomer(
+    				'customer_traking_order',
+    				$customer,
+    				[
+    						'order_id' => $order->getId(),
+    						'order_ref' => $order->getRef()
+    				]
+    				);
+    	}
+    	else {
+    		Tlog::getInstance()->info("Update order delivery ref - customer id: ".$order->getCustomerId().' was not found in database');
     	}
     }
 
     public static function getSubscribedEvents() 
     {
         return array(
-        		self::SEND_MAIL_IMPORTER=> array('sendEmailTrackingOrder', 128)
+        		self::SEND_MAIL_IMPORTER => array('sendEmailTrackingOrder', 128),
+        		TheliaEvents::ORDER_UPDATE_DELIVERY_REF => array('sendEmailTrackingOrderBackOffice', 128)
         );
     } 
 
