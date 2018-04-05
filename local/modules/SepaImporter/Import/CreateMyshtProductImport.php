1df62007c26a9764fa90099e3fc756171f293d35
alisstranici <alis.stranici@sepa.at>
2018-04-05 18:44:31 +0300

    don't import products with zero, vat for listen_price

--- local/modules/SepaImporter/Import/CreateMyshtProductImport.php
+++ local/modules/SepaImporter/Import/CreateMyshtProductImport.php
@@ -42,71 +42,79 @@ class CreateMyshtProductImport extends AbstractImport {
 	       		$errors .= 'Product '.$artNummer.' not found on mysht';
 	       }
 	       else {
-		       $session = $this->container->get('request_stack')->getCurrentRequest()->getSession();
-		       
-		       if($this->importedRows) { 
-		       		if($session->get('csvGenericProductImport')) {
-		       			$csv_file_name = $session->get('csvGenericProductImport');
-		       		}
+	       
+	       		if($infoProductMysht["resNettoRabatt"]["netto"] != '0,00' && $infoProductMysht["resNettoRabatt"]["netto"] != '0') {
+					
+		       	   $session = $this->container->get('request_stack')->getCurrentRequest()->getSession();
+			       if($this->importedRows) { 
+			       		if($session->get('csvGenericProductImport')) {
+			       			$csv_file_name = $session->get('csvGenericProductImport');
+			       		}
+			       }
+			       else {
+			       		$current_date = date("Y-m-d H:i:s");
+			       		$csv_file_name = 'genericProductImport_'.md5($current_date).'.csv';
+			       		$session->set('csvGenericProductImport', $csv_file_name);
+			       }
+			
+			       $filepath = THELIA_LOCAL_DIR . "sepa" . DS . "import" . DS . $csv_file_name;
+			      
+			       // mySht format 1.095,60 -> HF format 1095.60
+			       $price = str_replace(".", "", $infoProductMysht["resNettoRabatt"]["netto"]);
+			       $price = str_replace(",", ".", $price);
+			       $price = number_format($price, 2, '.', '');
+			      
+			       $listen_price = 1.2 * $infoProductMysht["data"][0]["aktpreis"];
+			       $listen_price = number_format($listen_price, 2, '.', '');
+			       
+			       $brand = $this->rowHasField($row,'Lief.Bezeichnung');
+			       
+			       $brandMatching = BrandMatchingPartnersQuery::create()
+			       						->findOneByBrandExtern($brand);
+			       						
+			       		
+			       if($brandMatching) {
+			       		$brandInternId = $brandMatching->getBrandIntern();
+			       		$brandCode = $brandMatching->getBrandCode();
+			       }
+			       else {
+			       		$brandInternId = '';
+			       		$brandCode = '';
+			       }
+			       
+			       $file_name = '';
+			       $imageLocation = THELIA_LOCAL_DIR . "media" . DS . "images" . DS . "importer" . DS . $infoProductMysht["data"][0]["MegabildNr"].'.jpg';
+			       if($infoProductMysht["data"][0]["MegabildNr"] && file_exists($imageLocation)) {
+			       		$file_name = $infoProductMysht["data"][0]["MegabildNr"].'.jpg';
+			       }
+			       		
+			       $arrayData = array(
+			       		"Extern_id" => $artNummer,
+			       		"Ref" => $brandCode.$artNummer,
+			       		"Marke_id" => $brandInternId,
+			       		"Kategorie_id" => '179',
+			       		"Produkt_titel" => $this->rowHasField($row,'Artikelbezeichnung'),
+			       		"Beschreibung" => $this->rowHasField($row,'Artikelbezeichnung'). " " . $this->rowHasField($row,'Artikelbezeichnung-2'),
+			       		"Menge" => $this->rowHasField($row,'Frei verw.'),
+			       		"Fulfilment_center" => MultipleFullfilmentCenters::getConfigValue('fulfilment_center_default'),
+			       		"EAN_code" =>  $this->rowHasField($row,'EAN'),
+			       		"Bild_file" => $file_name,
+			       		"Price" => $price,
+			       		"Listen_price" => $listen_price
+			       );
+			       
+			       if(file_exists($filepath)) {
+			       		$this->exportToCsv($filepath, $arrayData);
+			       		$this->importedRows++;
+			       }
+			       else {
+			       		$this->initCsvFile($filepath);
+			       		$this->exportToCsv($filepath, $arrayData);
+			       		$this->importedRows++;
+			       }
 		       }
 		       else {
-		       		$current_date = date("Y-m-d H:i:s");
-		       		$csv_file_name = 'genericProductImport_'.md5($current_date).'.csv';
-		       		$session->set('csvGenericProductImport', $csv_file_name);
-		       }
-		
-		       $filepath = THELIA_LOCAL_DIR . "sepa" . DS . "import" . DS . $csv_file_name;
-		      
-		       // mySht format 1.095,60 -> HF format 1095.60
-		       $price = str_replace(".", "", $infoProductMysht["resNettoRabatt"]["netto"]);
-		       $price = str_replace(",", ".", $price);
-		       $price = number_format($price, 2, '.', '');
-		       
-		       $listen_price = number_format($infoProductMysht["data"][0]["aktpreis"], 2, '.', '');
-		       $brand = $this->rowHasField($row,'Lief.Bezeichnung');
-		       
-		       $brandMatching = BrandMatchingPartnersQuery::create()
-		       						->findOneByBrandExtern($brand);
-		       						
-		       		
-		       if($brandMatching) {
-		       		$brandInternId = $brandMatching->getBrandIntern();
-		       		$brandCode = $brandMatching->getBrandCode();
-		       }
-		       else {
-		       		$brandInternId = '';
-		       		$brandCode = '';
-		       }
-		       
-		       $file_name = '';
-		       $imageLocation = THELIA_LOCAL_DIR . "media" . DS . "images" . DS . "importer" . DS . $infoProductMysht["data"][0]["MegabildNr"].'.jpg';
-		       if($infoProductMysht["data"][0]["MegabildNr"] && file_exists($imageLocation)) {
-		       		$file_name = $infoProductMysht["data"][0]["MegabildNr"].'.jpg';
-		       }
-		       		
-		       $arrayData = array(
-		       		"Extern_id" => $artNummer,
-		       		"Ref" => $brandCode.$artNummer,
-		       		"Marke_id" => $brandInternId,
-		       		"Kategorie_id" => '179',
-		       		"Produkt_titel" => $this->rowHasField($row,'Artikelbezeichnung'),
-		       		"Beschreibung" => $this->rowHasField($row,'Artikelbezeichnung'). " " . $this->rowHasField($row,'Artikelbezeichnung-2'),
-		       		"Menge" => $this->rowHasField($row,'Frei verw.'),
-		       		"Fulfilment_center" => MultipleFullfilmentCenters::getConfigValue('fulfilment_center_default'),
-		       		"EAN_code" =>  $this->rowHasField($row,'EAN'),
-		       		"Bild_file" => $file_name,
-		       		"Price" => $price,
-		       		"Listen_price" => $listen_price
-		       );
-		       
-		       if(file_exists($filepath)) {
-		       		$this->exportToCsv($filepath, $arrayData);
-		       		$this->importedRows++;
-		       }
-		       else {
-		       		$this->initCsvFile($filepath);
-		       		$this->exportToCsv($filepath, $arrayData);
-		       		$this->importedRows++;
+		       	$errors .= 'The product '.$this->rowHasField($row,'Artikel').' has no price';
 		       }
 	       }
 	       
