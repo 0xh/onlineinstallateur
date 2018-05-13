6e0eb0504f1dbc92ddb755547e0ed4f05e0cc6d3
Ajrose <aniarm21@yahoo.com>
2018-05-13 15:40:56 +0200

    outlet new design

---
+++ local/modules/Selection/Form/SelectionUpdateForm.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Selection\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Thelia\Core\Translation\Translator;
+use Thelia\Form\BaseForm;
+use Symfony\Component\Validator\Constraints;
+
+class SelectionUpdateForm extends BaseForm
+{
+    /**
+     *  Form build for add and update a selection
+     */
+    protected function buildForm()
+    {
+        $this->formBuilder
+            ->add(
+                'selection_id',
+                'text',
+                array(
+                    "constraints"   => array(
+                        new Constraints\NotBlank()
+                    ),
+                "label"         => 'Selection reference',
+                "required"      => false,
+                "read_only"     => true,
+                )
+            )
+            ->add(
+                'selection_title',
+                'text',
+                array(
+                    "constraints"   => array(
+                        new Constraints\NotBlank()
+                    ),
+                "label"         => Translator::getInstance()->trans('Title'),
+                "required"      => false,
+                )
+            )
+            ->add(
+                'selection_chapo',
+                TextareaType::class,
+                array(
+                    'attr'          => array('class' => 'tinymce'),
+                    "constraints"   => array(
+                    ),
+                "label"         =>Translator::getInstance()->trans('Summary'),
+                "required"      => false,
+                )
+            )
+            ->add(
+                'selection_header',
+                TextareaType::class,
+                array(
+                    'attr'          => array('class' => 'tinymce'),
+                    "constraints"   => array(
+                    ),
+                "label"         =>Translator::getInstance()->trans('Header'),
+                "required"      => false,
+                )
+            )
+            ->add(
+                'selection_footer',
+                TextareaType::class,
+                array(
+                    'attr'          => array('class' => 'tinymce'),
+                    "constraints"   => array(
+                    ),
+                "label"         =>Translator::getInstance()->trans('Footer'),
+                "required"      => false,
+                )
+            )
+            ->add(
+                'selection_postscriptum',
+                TextareaType::class,
+                array(
+                    'attr'          => array('class' => 'tinymce'),
+                    "constraints"   => array(
+                    ),
+                "label"         => Translator::getInstance()->trans('Conclusion'),
+                "required"      => false,
+                )
+            )
+            ->add(
+                'save_mode',
+                SubmitType::class,
+                array(
+                    'attr'          => array('class' => 'save'),
+                    'label'         =>'save',
+                    )
+            )
+            ->add(
+                'save_mode',
+                SubmitType::class,
+                array(
+                    'attr'          => array('class' => 'save_and_close'),
+                    'label'         =>'save_and_close'
+                    )
+            );
+    }
+
+    /**
+     * @return string the name of the form. This name need to be unique.
+     */
+    public function getName()
+    {
+        return "admin_selection_update";
+    }
+}
