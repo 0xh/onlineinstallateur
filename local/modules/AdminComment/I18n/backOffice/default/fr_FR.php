9302405f5704d2b8e4366b7f2187ca2ed4427a45
emanuelplopu <emanuel.plopu@sepa.at>
2018-05-30 15:30:46 +0200

    First commit of module

---
+++ local/modules/AdminComment/I18n/backOffice/default/fr_FR.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'Admin Comments' => 'Commentaires Admin',
+    'Delete this comment' => 'Supprimer le commentaire',
+    'Edit this comment' => 'Modifier ce commentaire',
+    'Message' => 'Message',
+    'Save' => 'Enregistrer',
+);
---
+++ local/modules/AdminComment/I18n/en_US.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'Admin Id' => 'Admin Id',
+    'Comment' => 'Comment',
+    'Comment Id' => 'Comment Id',
+    'Element id' => 'Element id',
+    'Element key' => 'Element key',
+    'Unknown comment' => 'Unknown comment',
+);
