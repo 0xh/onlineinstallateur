f133335b89d5093c28e6245de8389e49edcef1d9
cristelucian <36837223+cristelucian@users.noreply.github.com>
2018-09-27 16:50:21 +0300

    Merge EAS-75 to dev

--- local/I18n/de_DE.php
+++ local/I18n/de_DE.php
@@ -8,7 +8,9 @@ return [
     'PRODUCT OVERVIEW' => 'PRODUKTÜBERSICHT',
     'Price' => 'Preis',
     'Selection' => 'Auswahl',
+    'Selection Wish list' => ' Auswahl Wunschliste',
     'Total' => 'Gesamtsumme',
+    'You have no selection items in your wishlist.' => ' Sie haben keinen Artikel auf Ihrer Wunschliste.',
     'selection.fo.easybad' => [
         'Add to cart' => 'In den Warenkorb',
         'All items in this bathroom in detail' => 'Alle Artikel in diesem Badezimmer im Detail',
@@ -18,5 +20,9 @@ return [
         'Price' => 'Preis',
         'Selection' => 'Auswahl',
         'Total' => 'Gesamtsumme',
+    ],
+    'wishlist.fo.easybad' => [
+        'Selection Wish list' => ' Auswahl Wunschliste',
+        'You have no selection items in your wishlist.' => ' Sie haben keinen Artikel auf Ihrer Wunschliste.',
     ],
 ];
