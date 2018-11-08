ce56f47e57a93b7503a819757463fa1f09815130
emanuelplopu <emanuel.plopu@sepa.at>
2018-11-08 11:44:10 +0100

    add missing use

--- local/modules/Scraper/Controller/Scrapers/Megabad.php
+++ local/modules/Scraper/Controller/Scrapers/Megabad.php
@@ -2,6 +2,8 @@
 
 namespace Scraper\Controller\Scrapers;
 
+use Exception;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
