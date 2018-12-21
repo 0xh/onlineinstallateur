4756cbcadac1c0333196a7d67a745d0895fcc7eb
Adrian Padurean <securiteradmin@gmail.com>
2018-12-21 18:00:50 +0200

    Auto-trigger order_revenue update

--- local/modules/RevenueDashboard/Listener/OrderRevenueListener.php
+++ local/modules/RevenueDashboard/Listener/OrderRevenueListener.php
@@ -1,172 +1,213 @@
 <?php
-
-namespace RevenueDashboard\Listener;
-
-use MultipleFullfilmentCenters\Model\FulfilmentCenterOrderQuery;
-use MultipleFullfilmentCenters\Model\Map\FulfilmentCenterOrderTableMap;
-use MultipleFullfilmentCenters\Model\Map\FulfilmentCenterTableMap;
-use RevenueDashboard\Model\OrderProductRevenueQuery;
-use RevenueDashboard\Model\OrderRevenue;
-use RevenueDashboard\Model\OrderRevenueQuery;
-use RevenueDashboard\RevenueDashboard;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Thelia\Action\BaseAction;
-use Thelia\Core\Event\Order\OrderEvent;
-use Thelia\Core\Event\TheliaEvents;
-use Thelia\Core\HttpFoundation\Request;
-use Thelia\Log\Tlog;
-use Thelia\Model\OrderProductQuery;
-use Thelia\Model\OrderQuery;
-
-/**
- * Description of OrderRevenueListener
- *
- * @author Catana Florin
- */
-class OrderRevenueListener extends BaseAction implements EventSubscriberInterface {
-
-    protected $request;
     
-    public function __construct(Request $request) {
-        $this->request = $request;
-    }
+    namespace RevenueDashboard\Listener;
+    
+    use MultipleFullfilmentCenters\Model\FulfilmentCenterOrderQuery;
+    use MultipleFullfilmentCenters\Model\Map\FulfilmentCenterOrderTableMap;
+    use MultipleFullfilmentCenters\Model\Map\FulfilmentCenterTableMap;
+    use RevenueDashboard\Controller\Admin\OrderProductRevenueController;
+    use RevenueDashboard\Model\OrderProductRevenueQuery;
+    use RevenueDashboard\Model\OrderRevenue;
+    use RevenueDashboard\Model\OrderRevenueQuery;
+    use RevenueDashboard\Model\WholesalePartnerProductQuery;
+    use RevenueDashboard\RevenueDashboard;
+    use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+    use Thelia\Action\BaseAction;
+    use Thelia\Core\Event\Order\OrderEvent;
+    use Thelia\Core\Event\TheliaEvents;
+    use Thelia\Core\HttpFoundation\Request;
+    use Thelia\Model\OrderProductQuery;
+    use Thelia\Model\OrderQuery;
+    use Thelia\Model\ProductQuery;
 
-    protected function updateOrderRevenue($orderId) {
-        Tlog::getInstance()->debug("ORL: Update Order Revenue");
-        $orderProdRevenue = OrderProductRevenueQuery::create()
-                ->findByOrderId($orderId);
-        Tlog::getInstance()->debug("ORL: Order product revenue query ".$orderProdRevenue);
+    /**
+     * Description of OrderRevenueListener
+     *
+     * @author Catana Florin
+     */
+    class OrderRevenueListener extends BaseAction implements EventSubscriberInterface
+    {
         
+        protected $request;
         
-        $orderRevenue = OrderRevenueQuery::create()
-                ->findByOrderId($orderId);
-
-        Tlog::getInstance()->debug("ORL: Order revenue query ".$orderRevenue);
-        $paymentProcessorCost = $this->getDeliveryCostMethodPay($orderId);
-        $totalPaymentProcessorCost = $paymentProcessorCost["cost_module"] + $paymentProcessorCost["cost_transaction_module"];
-        $deliveryCostMethod = $this->getDeliveryCostMethod($orderId);
-
-        Tlog::getInstance()->debug("ORL: Payment processor Cost: ".$paymentProcessorCost." Total payment processor cost ".$totalPaymentProcessorCost." Delivery cost method ".$deliveryCostMethod);
+        public function __construct(Request $request)
+        {
+            $this->request = $request;
+        }
         
-        if ($orderRevenue->getData()) {
-            foreach ($orderRevenue as $orderRev) {
-                $orderRev->delete();
+        /**
+         * Returns an array of event names this subscriber wants to listen to.
+         *
+         * The array keys are event names and the value can be:
+         *
+         *  * The method name to call (priority defaults to 0)
+         *  * An array composed of the method name to call and the priority
+         *  * An array of arrays composed of the method names to call and respective
+         *    priorities, or 0 if unset
+         *
+         * For instance:
+         *
+         *  * array('eventName' => 'methodName')
+         *  * array('eventName' => array('methodName', $priority))
+         *  * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+         *
+         * @return array The event names to listen to
+         *
+         * @api
+         */
+        public static function getSubscribedEvents()
+        {
+            return array(
+                TheliaEvents::ORDER_UPDATE_STATUS => array("updateOrderStatus", 128),
+                TheliaEvents::ORDER_PRODUCT_AFTER_CREATE => array("updateWPPonOrderCreate", 128)
+            );
+        }
+        
+        public function updateOrderStatus(OrderEvent $event)
+        {
+            $completedStatus = 8;
+            $newStatus = $event->getStatus();
+            $order = $event->getOrder();
+            
+            if ($completedStatus == $newStatus) {
+                $this->updateOrderRevenue($order->getId());
             }
         }
-        foreach ($orderProdRevenue as $value) {
-            $this->addNewOrderRevenue($value, $paymentProcessorCost, $totalPaymentProcessorCost, $deliveryCostMethod);
+        
+        protected function updateOrderRevenue($orderId)
+        {
+            $orderProdRevenue = OrderProductRevenueQuery::create()
+                ->findByOrderId($orderId);
+            
+            $orderRevenue = OrderRevenueQuery::create()
+                ->findByOrderId($orderId);
+            
+            $paymentProcessorCost = $this->getDeliveryCostMethodPay($orderId);
+            $totalPaymentProcessorCost = $paymentProcessorCost["cost_module"] + $paymentProcessorCost["cost_transaction_module"];
+            $deliveryCostMethod = $this->getDeliveryCostMethod($orderId);
+            
+            if ($orderRevenue->getData()) {
+                foreach ($orderRevenue as $orderRev) {
+                    $orderRev->delete();
+                }
+            }
+            foreach ($orderProdRevenue as $value) {
+                $this->addNewOrderRevenue($value, $paymentProcessorCost, $totalPaymentProcessorCost, $deliveryCostMethod);
+            }
         }
-    }
-
-    protected function addNewOrderRevenue($orderProdRevenue, $paymentProcessorCost, $totalPaymentProcessorCost, $deliveryCostMethod) {
-        Tlog::getInstance()->debug("ORL: add new order revenue ");
-        $orderProduct = OrderProductQuery::create()
-        ->filterByOrderId($orderProdRevenue->getOrderId())
-        ->findOneByProductRef($orderProdRevenue->getProductRef());
         
-        $orderQuantity = $orderProduct->getQuantity();
-        $sellPrice = $orderProdRevenue->getPrice()*$orderQuantity;
-        $purchasePrice = $orderProdRevenue->getPurchasePrice()*$orderQuantity;
+        protected function getDeliveryCostMethodPay($orderId)
+        {
+            $order = OrderQuery::create()
+                ->findOneById($orderId);
+            
+            $payment_module_id = 0;
+            
+            if ($order) {
+                $payment_module_id = $order->getPaymentModuleId();
+            }
+            
+            return array(
+                "cost_module" => RevenueDashboard::getConfigValue('cost_module_' . $payment_module_id),
+                "cost_transaction_module" => RevenueDashboard::getConfigValue('cost_transaction_module_' . $payment_module_id)
+            );
+        }
         
-        $newOrderRevenue = new OrderRevenue();
-        $newOrderRevenue->setDeliveryCost($deliveryCostMethod["delivery_cost"]);
-        $newOrderRevenue->setOrderId($orderProdRevenue->getOrderId());
-        $newOrderRevenue->setDeliveryMethod($deliveryCostMethod["delivery_method"]);
-        $newOrderRevenue->setPartnerId($orderProdRevenue->getPartnerId());
-        $newOrderRevenue->setPaymentProcessorCost($totalPaymentProcessorCost);
-        $newOrderRevenue->setPrice($sellPrice);
-        $newOrderRevenue->setPurchasePrice($purchasePrice);
-        $totalPurchasePrice = $purchasePrice + $deliveryCostMethod["delivery_cost"] + $totalPaymentProcessorCost;
-
-        $newOrderRevenue->setTotalPurchasePrice($totalPurchasePrice);
-        $newOrderRevenue->setRevenue($sellPrice - $totalPurchasePrice);
-        $newOrderRevenue->save();
-    }
-
-    protected function getDeliveryCostMethod($orderId) {
-        Tlog::getInstance()->debug("ORL: get deliverly cost method");
-        $center = FulfilmentCenterOrderQuery::create()
+        protected function getDeliveryCostMethod($orderId)
+        {
+            $center = FulfilmentCenterOrderQuery::create()
                 ->addJoin(FulfilmentCenterOrderTableMap::CENTER_ID, FulfilmentCenterTableMap::ID)
                 ->withColumn(FulfilmentCenterTableMap::DELIVERY_COST, 'delivery_cost')
                 ->withColumn(FulfilmentCenterTableMap::DELIVERY_METHOD, 'delivery_method')
                 ->findOneByOrderId($orderId);
-
-        if ($center) {
+            
+            if ($center) {
+                return array(
+                    "delivery_cost" => $center->getVirtualColumn("delivery_cost"),
+                    "delivery_method" => $center->getVirtualColumn("delivery_method")
+                );
+            }
+            
             return array(
-                "delivery_cost" => $center->getVirtualColumn("delivery_cost"),
-                "delivery_method" => $center->getVirtualColumn("delivery_method")
+                "delivery_cost" => RevenueDashboard::getConfigValue('delivery_cost'),
+                "delivery_method" => RevenueDashboard::getConfigValue('delivery_method')
             );
         }
-
-        return array(
-            "delivery_cost" => RevenueDashboard::getConfigValue('delivery_cost'),
-            "delivery_method" => RevenueDashboard::getConfigValue('delivery_method')
-        );
-    }
-
-    protected function getDeliveryCostMethodPay($orderId) {
-        Tlog::getInstance()->debug("ORL: get deliverly cost method pay");
-        $order = OrderQuery::create()
-                ->findOneById($orderId);
-
-        $payment_module_id = 0;
-
-        if ($order) {
-            $payment_module_id = $order->getPaymentModuleId();
+        
+        protected function addNewOrderRevenue($orderProdRevenue, $paymentProcessorCost, $totalPaymentProcessorCost, $deliveryCostMethod)
+        {
+            $orderProduct = OrderProductQuery::create()
+                ->filterByOrderId($orderProdRevenue->getOrderId())
+                ->findOneByProductRef($orderProdRevenue->getProductRef());
+            
+            $orderQuantity = $orderProduct->getQuantity();
+            $sellPrice = $orderProdRevenue->getPrice() * $orderQuantity;
+            $purchasePrice = $orderProdRevenue->getPurchasePrice() * $orderQuantity;
+            
+            $newOrderRevenue = new OrderRevenue();
+            $newOrderRevenue->setDeliveryCost($deliveryCostMethod["delivery_cost"]);
+            $newOrderRevenue->setOrderId($orderProdRevenue->getOrderId());
+            $newOrderRevenue->setDeliveryMethod($deliveryCostMethod["delivery_method"]);
+            $newOrderRevenue->setPartnerId($orderProdRevenue->getPartnerId());
+            $newOrderRevenue->setPaymentProcessorCost($totalPaymentProcessorCost);
+            $newOrderRevenue->setPrice($sellPrice);
+            $newOrderRevenue->setPurchasePrice($purchasePrice);
+            $totalPurchasePrice = $purchasePrice + $deliveryCostMethod["delivery_cost"] + $totalPaymentProcessorCost;
+            
+            $newOrderRevenue->setTotalPurchasePrice($totalPurchasePrice);
+            $newOrderRevenue->setRevenue($sellPrice - $totalPurchasePrice);
+            $newOrderRevenue->save();
         }
+        
+        public function updateWPPonOrderCreate(OrderEvent $event)
+        {
+            $orderStatus = $event->getStatus();
+            $order = $event->getOrder();
+            $order_id = $order->getId();
+            
+            $oprc = new OrderProductRevenueController();
+            
+            $order_query = OrderQuery::create()
+                ->findOneById($order_id);
+            
+            $order_query_id = $order_query->getId();
+            
+            $orderProducts = null;
+            $orderProductQuery = OrderProductQuery::create();
+            $orderProducts = $orderProductQuery->findByOrderId($order_id);
+            
+            $wholesalePartnerProd = new WholesalePartnerProductQuery();
+            
+            
+            foreach ($orderProducts as $orderProduct) {
+                $order_query_prodRef = $orderProduct->getProductRef();
+                
+                $prod_query = ProductQuery::create();
+                $prod_id = $prod_query->findOneByRef($order_query_prodRef);
+                
+                $order_query_price = $orderProduct->getPrice();
+                $order_query_partnerId = 1;
+                $order_query_prodId = $prod_id->getId();
 
-        return array(
-            "cost_module" => RevenueDashboard::getConfigValue('cost_module_' . $payment_module_id),
-            "cost_transaction_module" => RevenueDashboard::getConfigValue('cost_transaction_module_' . $payment_module_id)
-        );
-    }
-
-    public function updateOrderStatus(OrderEvent $event) {
-       Tlog::getInstance()->debug("ORL: update order status"); 
-        $completedStatus = 8;
-        $newStatus = $event->getStatus();
-        $order = $event->getOrder();
-
-        if ($completedStatus == $newStatus) {
-            $this->updateOrderRevenue($order->getId());
+//                echo $order_query_prodId;
+                
+                $wholesalePartnerProd->filterByProductId($order_query_prodId);
+                $wpp_item = $wholesalePartnerProd->findOne();
+                
+                if ($wpp_item != null) {
+                    $order_query_purchasePrice = $wpp_item->getPrice();
+//
+//                    echo "orderID: " . $order_id . " Order Prod REf: " . $order_query_prodRef . " product price: " . $order_query_price . " purchase price: " . $order_query_purchasePrice
+//                        . " order partner id " . $order_query_partnerId . " prod id: " .
+//                        $order_query_prodId;
+                    
+                    
+                    $oprc->updateOrderProductRevenue($order_id, $order_query_prodRef, $order_query_price, $order_query_purchasePrice, $order_query_partnerId, $order_query_prodId);
+                }
+            }
+            
+            
+            $this->updateOrderRevenue($order_id);
         }
+        
     }
-    
-    public function updateWPPonOrderCreate(OrderEvent $event) {
-       Tlog::getInstance()->debug("ORL: updateWPPonOrderCreate"); 
-        $orderStatus = $event->getStatus();
-        $order = $event->getOrder();
-
-            $this->updateOrderRevenue($order->getId());
-    }
-    
-
-
-    /**
-     * Returns an array of event names this subscriber wants to listen to.
-     *
-     * The array keys are event names and the value can be:
-     *
-     *  * The method name to call (priority defaults to 0)
-     *  * An array composed of the method name to call and the priority
-     *  * An array of arrays composed of the method names to call and respective
-     *    priorities, or 0 if unset
-     *
-     * For instance:
-     *
-     *  * array('eventName' => 'methodName')
-     *  * array('eventName' => array('methodName', $priority))
-     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
-     *
-     * @return array The event names to listen to
-     *
-     * @api
-     */
-    public static function getSubscribedEvents() {
-        return array(
-            TheliaEvents::ORDER_UPDATE_STATUS => array("updateOrderStatus", 128),
-            TheliaEvents::ORDER_UPDATE_STATUS => array("updateWPPonOrderCreate", 128)
-        );
-    }
-
-}
